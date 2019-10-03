cc4e42badb6b524175984c9f0429cc177d864dd8
kyokonishito <nishito@jp.ibm.com>
2019-10-03 10:26:38 +0900

    Update app.py VisualRecognitionV3 implementation

--- server/app.py
+++ server/app.py
@@ -4,24 +4,25 @@ from ibm_watson import VisualRecognitionV3
 import json
 
 VISUAL_RECOGNITION_BIND_PATH = '/opt/vr-service-bind/binding'
+VISUAL_RECOGNITION_API_VERSION = '2018-03-19'
 
 app = Flask(__name__, static_url_path='')
 app.config.from_object('config.default')
 if os.getenv('APP_CONFIG_FILE'):
     app.config.from_envvar('APP_CONFIG_FILE')
 
-
-visual_recognition = VisualRecognitionV3(
-    version='2018-03-19'
-)
-
 if os.path.exists(VISUAL_RECOGNITION_BIND_PATH): # For IKS
     with open(VISUAL_RECOGNITION_BIND_PATH, 'r') as conffile:
         confData = json.load(conffile)
         visual_recognition = VisualRecognitionV3(
-            version='2018-03-19',
-            iam_apikey= confData['apikey']
+            version = VISUAL_RECOGNITION_API_VERSION,
+            iam_apikey = confData['apikey']
         )
+else:
+    visual_recognition = VisualRecognitionV3(
+        version = VISUAL_RECOGNITION_API_VERSION
+    )  
+     
 
 
 @app.route('/')
